0675446a7aafbb45ec22bd6a08123e31f4ac5ebd
Darya Shevareva <140635378+Dasheeer@users.noreply.github.com>
2024-03-27 11:40:34 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,123 +11,124 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.blueGrey),
-      home: Scaffold(
-        body: Align(
+        theme: ThemeData(primaryColor: Colors.blueGrey),
+        home: Scaffold(
+            body: Align(
 
 
 
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
 
 
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    children: [
-                      Expanded(flex: 4, child:
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Expanded(flex: 4, child:
 
-                      Container(
+                          Container(
 
-                        alignment: Alignment.center,
-                        child: Text('1', style: TextStyle(fontSize: 36),),
+                            alignment: Alignment.center,
+                            child: Text('1', style: TextStyle(fontSize: 36),),
 
-                        color: Color.fromRGBO(243, 33, 156, 1),
-                        margin: EdgeInsets.only(bottom: 20, right: 8, top: 20, left: 15),
+                            color: Color.fromRGBO(243, 33, 156, 1),
 
-                      )),
-                      Expanded(flex: 1, child:
+                            margin: EdgeInsets.all(8),
 
-                      Container(
-                        alignment: Alignment.center,
-                        child: Text('2', style: TextStyle(fontSize: 36),),
+                          )),
+                          Expanded(flex: 1, child:
 
-                        color: Color.fromRGBO(88, 46, 205, 1),
-                        margin: EdgeInsets.only(top: 20, bottom: 20, right: 8, left: 15),
+                          Container(
+                            alignment: Alignment.center,
+                            child: Text('2', style: TextStyle(fontSize: 36),),
 
-                      )),
-                      Expanded(flex: 1, child:
+                            color: Color.fromRGBO(88, 46, 205, 1),
+                            margin: EdgeInsets.all(8),
 
-                      Container(
-                        alignment: Alignment.center,
-                        child: Text('3', style: TextStyle(fontSize: 36),),
+                          )),
+                          Expanded(flex: 1, child:
 
-                        color: Color.fromARGB(115, 121, 162, 161),
-                        margin: EdgeInsets.only(top: 20, bottom: 20, right: 20, left: 15),
+                          Container(
+                            alignment: Alignment.center,
+                            child: Text('3', style: TextStyle(fontSize: 36),),
 
-                      )),
-                      Expanded(flex: 4, child:
+                            color: Color.fromARGB(115, 121, 162, 161),
+                            margin: EdgeInsets.all(8),
 
-                      Container(
-                        alignment: Alignment.center,
-                        child: Text('4', style: TextStyle(fontSize: 36),),
+                          )),
+                          Expanded(flex: 4, child:
 
-                        color: Color.fromARGB(255, 117, 125, 119),
-                        margin: EdgeInsets.only(top: 20, bottom: 20, right: 15, left: 15),
+                          Container(
+                            alignment: Alignment.center,
+                            child: Text('4', style: TextStyle(fontSize: 36),),
 
-                      ),),],),),
-                Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        Expanded(flex: 4, child:
+                            color: Color.fromARGB(255, 117, 125, 119),
+                            margin: EdgeInsets.all(8),
 
-                        Container(
-                          alignment: Alignment.center,
-                          child: Text('5', style: TextStyle(fontSize: 36),),
+                          ),),],),),
+                    Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: [
+                            Expanded(flex: 4, child:
 
-                          color: Colors.amberAccent,
-                          margin: EdgeInsets.only(top: 20, bottom: 20, right: 15, left: 20),
+                            Container(
+                              alignment: Alignment.center,
+                              child: Text('5', style: TextStyle(fontSize: 36),),
 
-                        )),
-                        Expanded(flex: 1, child:
+                              color: Colors.amberAccent,
+                              margin: EdgeInsets.all(8),
 
-                        Container(
-                          alignment: Alignment.center,
-                          child: Text('6', style: TextStyle(fontSize: 36),),
+                            )),
+                            Expanded(flex: 1, child:
 
-                          color: Color.fromARGB(255, 128, 8, 198),
-                          margin: EdgeInsets.only(top: 20, bottom: 20, right: 15,  left: 20),
+                            Container(
+                              alignment: Alignment.center,
+                              child: Text('6', style: TextStyle(fontSize: 36),),
 
-                        )),
-                        Expanded(flex: 1, child:
+                              color: Color.fromARGB(255, 128, 8, 198),
+                              margin: EdgeInsets.all(8),
 
-                        Container(
-                          alignment: Alignment.center,
-                          child: Text('7', style: TextStyle(fontSize: 36),),
+                            )),
+                            Expanded(flex: 1, child:
 
-                          color: Color.fromARGB(255, 254, 29, 29),
-                          margin: EdgeInsets.only(top: 20, bottom: 20, right: 15,  left: 20),
+                            Container(
+                              alignment: Alignment.center,
+                              child: Text('7', style: TextStyle(fontSize: 36),),
 
-                        )),
-                        Expanded(flex: 4, child:
+                              color: Color.fromARGB(255, 254, 29, 29),
+                              margin: EdgeInsets.all(8),
 
-                        Container(
-                          alignment: Alignment.center,
-                          child: Text('8', style: TextStyle(fontSize: 36),),
+                            )),
+                            Expanded(flex: 4, child:
 
-                          color: Color.fromARGB(255, 110, 117, 158),
-                          margin: EdgeInsets.only(top: 20, bottom: 20, right: 15, left: 20),
-                        ),),],)
+                            Container(
+                              alignment: Alignment.center,
+                              child: Text('8', style: TextStyle(fontSize: 36),),
 
+                              color: Color.fromARGB(255, 110, 117, 158),
+                              margin: EdgeInsets.all(8),
+                            ),),],)
+
+
+                    ),
+
+                  ],
 
                 ),
 
-              ],
-
-            ),
 
 
 
+              ),
+            )
 
-          ),
+
         )
-
-
-      )
     );
   }
 }
